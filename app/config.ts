fe8c4e7e73b43ae42b4e42c898d6ddc6ec0f3bd3
Rodrigo Ribeiro <me@rodrigo3d.com>
2025-04-21 22:10:02 -0300

    feat: main update

--- app/config.ts
+++ app/config.ts
@@ -4,13 +4,13 @@ export const metaData = {
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance."
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
+  twitter: "",
   github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
+  instagram: "",
   linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  email: "mailto:example@gmail.com"
 };
